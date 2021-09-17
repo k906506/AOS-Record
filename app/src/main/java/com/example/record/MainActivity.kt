43d5084817a0204c749d51641c0b7dae6944824c
k906506 <k906506@naver.com>
2021-09-17 16:29:34 +0900

    사용자에게 권한을 요청하는 메소드 추가

--- app/src/main/java/com/example/record/MainActivity.kt
+++ app/src/main/java/com/example/record/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.example.record
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private val recordButton: RecordButton by lazy {
+        findViewById(R.id.recordButton)
+    }
+
+    private val requiredPermissions = arrayOf(Manifest.permission.RECORD_AUDIO)
+
+    private var state = State.BEFORE_RECORDING
+
+    // 정적변수로 만들어주기 위해 companion 객체 사용
+    companion object {
+        private const val REQUEST_RECORD_AUDIO_PERMISSION = 201
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews()
+        requestAudioPermission()
+    }
+
+    private fun initViews() {
+        recordButton.updateIconWithState(state)
+    }
+
+    private fun requestAudioPermission() {
+        requestPermissions(requiredPermissions, REQUEST_RECORD_AUDIO_PERMISSION)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        val audioRecordPermissionGranted =
+            requestCode == REQUEST_RECORD_AUDIO_PERMISSION && grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED
+
+        if (!audioRecordPermissionGranted) {
+            finish()
+        }
     }
 }
